3e2fcd01990ea90815577addb01079eddb10053c
geraldgsh <geraldgsh@hotmail.com>
2020-09-21 23:47:34 +0800

    dialog setup

--- src/components/resultPage.jsx
+++ src/components/resultPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
@@ -7,7 +8,16 @@ import {
 import Avatar from '@material-ui/core/Avatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import { BugReportSharp } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,7 +32,120 @@ const useStyles = makeStyles(theme => ({
     justifyContent: 'center',
     padding: '10px',
   },
+  media: {
+    minHeight: '294px',
+    minWidth: '294px',
+  },
 }));
+
+const styles = theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)(props => {
+  const {
+    children, classes, onClose, ...other
+  } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles(theme => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles(theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+const CaseNote = info => {
+  const classes = useStyles();
+  const theft = new Date(info.info.occurred_at * 1000).toDateString();
+  const reported = new Date(info.info.updated_at * 1000).toDateString();
+  console.group(info.info.media.image_url);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Details
+      </Button>
+      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle>
+          {info.info.title}
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            {
+              info.info.description
+                ? info.info.description
+                : <em>No description given</em>
+            }
+            <br />
+            <br />
+          </Typography>
+          <Typography gutterBottom>
+            Date of the theft:
+            {' '}
+            <b>{theft}</b>
+          </Typography>
+          <Typography gutterBottom>
+            Date of report:
+            {' '}
+            <b>{reported}</b>
+          </Typography>
+          <br />
+          {
+            info.info.media.image_url
+              ? <CardMedia className={classes.media} image={info.info.media.image_url} />
+              : <em>No image provided</em>
+          }
+          <br />
+          <Typography gutterBottom>
+            <br />
+            Location of theft:
+            {' '}
+            <u>{info.info.address}</u>
+            <br />
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 
 export default function SideBar() {
   const classes = useStyles();
@@ -36,8 +159,6 @@ export default function SideBar() {
             return (
               <ListItem
                 key={report.id}
-                button
-                onClick={() => console.log(report.id)}
               >
                 <ListItemAvatar>
                   <Avatar
@@ -52,6 +173,7 @@ export default function SideBar() {
                   secondary={report.description}
                   className={classes.item}
                 />
+                <CaseNote info={report} />
               </ListItem>
             );
           }) : null}
