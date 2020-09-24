b96dffa0ef2bf71efd48a660ffcab70ac9755eab
geraldgsh <geraldgsh@hotmail.com>
2020-09-24 16:16:41 +0800

    moved more styles

--- src/components/dialog/dialogHelper.jsx
+++ src/components/dialog/dialogHelper.jsx
@@ -5,19 +5,7 @@ import Typography from '@material-ui/core/Typography';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
-
-const styles = theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
+import styles from '../../assets/dialogHelperStyles';
 
 export const DialogTitle = withStyles(styles)(props => {
   const {
